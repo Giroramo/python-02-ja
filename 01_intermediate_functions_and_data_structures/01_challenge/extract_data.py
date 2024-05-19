84b44254e722d5db420258e88b0faaf168167f1c
Jiro Fujino <jiro.fujino@woven-planet.global>
2024-05-19 21:24:22 +0900

    Submit challenge for grading

--- 01_intermediate_functions_and_data_structures/01_challenge/extract_data.py
+++ 01_intermediate_functions_and_data_structures/01_challenge/extract_data.py
@@ -1,7 +1,15 @@
 import re
 
 def extract_data(text):
-    pass
+    # 正規表現パターン: コロンで区切られたデータ名とその値をキャプチャ
+    pattern = r'\w+:[^\s,.;]+'
+    # 正規表現に一致する部分をすべて抽出
+    matches = re.findall(pattern, text)
+    return matches
 
 def better_extract_data(text):
-    pass
+    # 正規表現パターン: データ名とその値をキャプチャ
+    pattern = r'(\w+):([^\s,.;]+)'
+    # 正規表現に一致する部分をすべて抽出
+    matches = re.findall(pattern, text)
+    return matches
